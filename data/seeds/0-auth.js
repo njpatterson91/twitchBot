f3b7f44ff1adf9c95872b30a4323c726db988c8f
Nathaniel Patterson <odst0016@gmail.com>
2021-07-02 16:31:57 -0400

    fixed seed file

--- data/seeds/0-auth.js
+++ data/seeds/0-auth.js
@@ -1,14 +1,14 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex("table_name")
+  return knex("auth")
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex("table_name").insert([
+      return knex("auth").insert([
         { id: 1, name: "username", value: "" },
         { id: 2, name: "password", value: "" },
         { id: 3, name: "channel", value: "" },
-        { id: 3, name: "weather", value: "" },
+        { id: 4, name: "weather", value: "" },
       ]);
     });
 };
